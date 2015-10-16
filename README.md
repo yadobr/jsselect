aa729785c4418a66f16d42938f6f0e698101f4f8
yadobr <php.dev@mail.ru>
2015-10-16 16:21:06 +0800

    Disable attr support

---
+++ README.md
@@ -0,0 +1,96 @@
+jsselect
+========
+
+jsselect.js is a JQuery plugin for creating styleable select elements.
+Features:
+	0. Auto width
+	0. Themes
+	0. Tabindex and :focus support
+	0. Disabled attr support
+	0. Cut off long text if width set manualy. And tooltip with full text
+	0. Select manualy refresh: $.fn.jsselect.refresh('#sel')
+	0. In future: json data source, multiselect, arrow keys navigation etc.
+
+Usage
+-------
+In html file in <head>:
+```html
+    <link href="css/jsselect.css" rel="styleSheet">
+    <link href="css/jsselect.theme.default.css" rel="styleSheet">
+```
+In html file before <body> end:
+```html
+    <script src="js/jquery.min.js"></script>
+    <script src="js/jsselect.js"></script>
+    <script>
+      $('#sel').jsselect({
+         debug: false,
+         oncreate: function(){ console.log('Create') },
+         onfocus: function(){ console.log('Focus') },
+         onopen: function(){ console.log('Open') },
+         onchange: function(){ console.log('Change') },
+         onblur: function(){ console.log('Blur') },
+         onclose: function(){ console.log('Close') }
+      });
+    </script>
+```
+
+Example
+-------
+```html
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>jsselect</title>
+    <meta charset="utf-8">
+    <link href="css/jsselect.css" rel="styleSheet">
+    <link href="css/jsselect.theme.default.css" rel="styleSheet">
+  </head>
+  <body>
+    <select id="sel">
+      <option>One</option>
+      <option selected>Two</option>
+      <option>Three</option>
+    </select>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/jsselect.js"></script>
+    <script>
+      $('#sel').jsselect({
+         debug: false,
+		 tabindex: 0,
+         oncreate: function(){ console.log('Create') },
+         onfocus: function(){ console.log('Focus') },
+         onopen: function(){ console.log('Open') },
+         onchange: function(){ console.log('Change') },
+         onblur: function(){ console.log('Blur') },
+         onclose: function(){ console.log('Close') }
+      });
+    </script>
+  </body>
+</html>
+```
+	
+Options
+-------	
+* `debug` values: `true|false` -- If `true` display source select element
+* `width` values: `pixels|percentage` -- Set jsselect width. If the property is not set then width = auto by default
+* `tabindex` values: `number` -- Set tabindex attribute
+
+Events
+------
+* `oncreate(oldSelect, newSelect, header, list)`
+* `onfocus(e)`
+* `onopen(e, oldSelect, newSelect, header, list)`
+* `onchange(e, elm, ind, oldSelect, newSelect, header, list)` -- `elm` - It's a selected option in old select. And `ind` - index of elm
+* `onblur(e)`
+* `onclose(oldSelect, newSelect, header, list)`
+
+Methods
+-------
+* `$.fn.jsselect.refresh(selectorOfOldSelect)` - Refresh header and list in newSelect. May be used after:
+```javascript
+$($('#sel').children()[0]).prop('selected', true)
+```
+And etc.
+* `$.fn.jsselect.enable(selector)` - Enable newSelect and oldSelect
+* `$.fn.jsselect.disable(selector)` - Disable newSelect and oldSelect
--- js/jsselect.js
+++ js/jsselect.js
@@ -1,91 +1,161 @@
-var jsselectInstanceCount = 0; // Count of jsselect instance
-
-$.fn.jsselect = function(){
-
-    //region Init
-    var src,
-        inp,
-        lbl,
-        ul;
-
-    jsselectInstanceCount++;
-    src = this;
-    inp = $('<input id="jsselect'+jsselectInstanceCount+'" class="jsselect" type="checkbox">');
-    lbl = $('<label for="jsselect'+jsselectInstanceCount+'">');
-
-    src
-        .after(lbl)
-        .after(inp);
-
-    src.css({
-        'font-family': lbl.css('font-family'),
-        'font-size': lbl.css('font-size')
-    });
-
-    inp.css({'left': lbl.position().left, 'top': lbl.position().top});
-
-    lbl.width(src.outerWidth());
-    lbl.html(src.find(':selected').html());
+$.fn.jsselect = function(prop){
+    //region VARIABLES
+    var newSelect = $('<div class="jsselect">'),
+        oldSelect = this,
+        header = $('<p>'),
+        list = $('<ul>'),
+        cb = $('<input type="checkbox">'),
+        isOnCreate = false,
+        isOnFocus  = false,
+        isOnOpen   = false,
+        isOnChange = false,
+        isOnBlur   = false,
+        isOnClose  = false;
     //endregion
 
-    // Open created select
-    lbl.click(open);
+    if(oldSelect.length != 0){
+
+        //region CREATE NEW SELECT
+        // Append container
+        newSelect.insertAfter(oldSelect);
+
+        // Append elements
+        cb.appendTo(newSelect);
+        header.appendTo(newSelect);
+        list.appendTo(newSelect);
+
+        // If oldSelect was disabled
+        oldSelect.prop('disabled') && newSelect.addClass('jsselect-disabled')
+
+        // Find selected by default option in oldSelect and add to newSelect
+        setHeader();
+
+        // Find other <option> and add to list
+        setList();
+        //endregion
+
+        //region SET PROPERTIES
+        // If overwritten
+        if(typeof prop == 'object'){
+
+            // Debug
+            prop.debug == true ? oldSelect.show() : oldSelect.hide();
+
+            // Width
+            prop.width ? (newSelect.css('width', prop.width) && list.css('width', prop.width) ) : newSelect.css('width', list.width());
+
+            // Debug
+            typeof prop.tabIndex == 'number' && cb.attr('tabindex', prop.tabIndex);
+
+            // oncreate
+            typeof prop.oncreate == 'function' && (isOnCreate = true);
+
+            // onfocus
+            typeof prop.onfocus == 'function' && (isOnFocus = true);
+
+            // onopen
+            typeof prop.onopen == 'function' && (isOnOpen = true);
+
+            // onchange
+            typeof prop.onchange == 'function' && (isOnChange = true);
+
+            // onblur
+            typeof prop.onblur == 'function' && (isOnBlur = true);
+
+            // Onremove
+            typeof prop.onclose == 'function' && (isOnClose = true);
+        }
+
+        // By dedault
+        else{
+
+            // Debug
+            oldSelect.hide();
+
+            // Width
+            newSelect.css('width', list.width())
+        }
+        //endregion
+
+        //region PRIVATE EVENTS
+        // Set newSelect events
+        header.click(open);
+        list.click(change);
+
+        // Set checkbox events
+        cb.focus(cbFocus);
+        cb.change(cbChange);
+        cb.blur(cbBlur);
+
+        // Set oldSelect events
+        oldSelect.change(onOldSelectChange);
+        //endregion
+
+        isOnCreate && prop.oncreate(oldSelect, newSelect, header, list);
+    }
+
+    function setHeader(){
+        var html = oldSelect.find('> option:selected').html()
+        header
+            .html( html )
+            .attr('title', html);
+    }
+    function setList(){
+        oldSelect.find('option').each(function(){
+            $('<li>')
+                .html( $(this).html())
+                .attr('title', $(this).html())
+                .appendTo( list );
+        });
+    }
 
     function open(e){
         var x,
             y;
 
-        inp.addClass('jsselect-focused');
+        if(!newSelect.hasClass('jsselect-disabled')) {
+            if (!newSelect.hasClass('jsselect-opened')) {
+                newSelect
+                    .addClass('jsselect-opened')
+                    .addClass('jsselect-focused');
 
-        ul = $('<ul class="jsselect-ul">');
+                x = newSelect.offset().left;
+                y = newSelect.position().top + newSelect.outerHeight() - parseInt(newSelect.css('border-top-width'), 10);
 
-        src.find('option').each(function(){
-            $('<li>')
-                .html( $(this).html() )
-                .appendTo(ul)
-                .click(select)
-        });
+                list.css({'left': x, 'top': y});
 
-        x = lbl.offset().left;
-        y = lbl.position().top + lbl.outerHeight() - parseInt(lbl.css('border-top-width'), 10);
+                isOnOpen && prop.onopen(e, oldSelect, newSelect, header, list);
 
-        ul.css({'left': x, 'top': y});
-
-        ul.width( lbl.css('width') );
-
-        $('body').append(ul);
-        ul.show();
-
-        ul.mouseout(function(){
-            window.addEventListener('click', close, true);
-        });
+                window.addEventListener('click', close, true);
+            } else {
+                newSelect.removeClass('jsselect-opened');
+                window.removeEventListener('click', close, true);
+            }
+        }
     }
-
-    function select(e){
-        var li  = $(e.target), // Clicked li
-            ind = li.index(),  // Clicked li position in ul
-            opt = $(src.children()[ind]); // Source select option
+    function change(e){
+        var elm = $(e.target), // Clicked li
+            ind = elm.index(), // Clicked li position in ul
+            opt = $(oldSelect.children()[ind]); // select option
 
         // li text to header
-        lbl.html( li.html() );
+        header.html( elm.html() );
 
-        // Add selected attribute to option in source select
+        // Add selected attribute to option in select
         opt.prop('selected', true);
 
-        // Broadcast source event
-        src.change();
+        oldSelect.change();
+        isOnClose && prop.onclose(oldSelect, newSelect, header, list);
 
-        // Remove ul
-        ul.remove();
-
-        // Set focus to label
-        lbl.focus();
+        // Hide list
+        newSelect.removeClass('jsselect-opened');
+        window.removeEventListener( 'click', close, true );
+        window.addEventListener( 'click', removeFocus, true );
     }
-
     function close(e){
         var child = false;
 
-        ul.find('*').each(function(){
+        list.find('*').each(function(){
             if(this == e.target){
                 child = true;
                 return false;
@@ -93,17 +163,81 @@ $.fn.jsselect = function(){
         });
 
         if( !child ){
-
-            // Remove class
-            inp.removeClass('jsselect-focused');
-
-            // Remove ul
-            ul.remove();
+            newSelect
+                .removeClass('jsselect-opened')
+                .removeClass('jsselect-focused');
 
             window.removeEventListener( 'click', close, true );
+            isOnClose && prop.onclose(oldSelect, newSelect, header, list);
 
             e.stopPropagation();
             return false;
         }
     }
+    function removeFocus(e){
+        newSelect.removeClass('jsselect-focused');
+        window.removeEventListener( 'click', removeFocus, true );
+    }
+
+    function cbFocus(e){
+        newSelect.addClass('jsselect-focused');
+        isOnFocus && prop.onfocus(e);
+    }
+    function cbChange(e){
+        if(this.checked)
+            open();
+    }
+    function cbBlur(e){
+        newSelect.removeClass('jsselect-focused');
+        cb.prop('checked', false);
+        isOnBlur && prop.onblur(e);
+    }
+
+    function onOldSelectChange(e){
+        var elm = oldSelect.find(':selected'), // Selected option in select
+            ind = elm.index();                 // Position of selected option in select
+
+        header.html( $(list.children()[ind]).html() );
+        isOnChange && prop.onchange(e, elm, ind, oldSelect, newSelect, header, list);
+    }
+}
+
+$.fn.jsselect.refresh = function(selector){
+    var oldSelect = $(selector),
+        newSelect = oldSelect.next(),
+        header,
+        list;
+
+    if(oldSelect.length != 0 && newSelect.length != 0){
+        var html;
+
+        // Refresh header
+        header = newSelect.find('> p');
+        html = oldSelect.find('> option:selected').html()
+        header
+            .html( html )
+            .attr('title', html);
+
+        // Refresh list
+        list = newSelect.find('> ul');
+        list.html('');
+        oldSelect.find('option').each(function(){
+            $('<li>')
+                .html( $(this).html())
+                .attr('title', $(this).html())
+                .appendTo( list );
+        });
+    }
+}
+$.fn.jsselect.enable = function(selector){
+    var oldSelect = $(selector);
+        newSelect = oldSelect.next();
+    oldSelect.prop('disabled', false);
+    newSelect.removeClass('jsselect-disabled');
+}
+$.fn.jsselect.disable = function(selector){
+    var oldSelect = $(selector);
+    newSelect = oldSelect.next();
+    oldSelect.prop('disabled', true);
+    newSelect.addClass('jsselect-disabled');
 }
